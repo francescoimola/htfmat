5a2af9ef07cd68e0e54769e34fb767e299649929
francescoimola <frn.imola@gmail.com>
2020-12-31 16:40:00 +0000

    done

--- app/av.js
+++ app/av.js
@@ -10,10 +10,12 @@ var myp5;
 
 //Tone.js and p5.js code
 var sketch = function (p) {
-  // Master volume in decibels
-  const volume = -2;
 
-  // The synth we will use for audio
+  // Set volume in decibels
+  const volume = -2;
+  Tone.Master.volume.value = volume;
+
+  //Variables for The synth we will use
   let synth;
   let risoColors;
   let colorJSON;
@@ -28,7 +30,11 @@ var sketch = function (p) {
   p.preload = function () {
     // loads a JSON as an object
     colorJSON = p.loadJSON("./tools/crayola.json");
+    preloadScale();
+  };
 
+  //Preload musical scale
+  function preloadScale() {
     //Preload scale from which to play notes
     let myscale = p.random(Tonal.Scale.names());
     console.log(myscale);
@@ -42,6 +48,17 @@ var sketch = function (p) {
 
   // Create a new canvas to the browser size
   p.setup = function () {
+    makeCanvas();
+    // unpack the JSON object as an array
+    risoColors = Object.values(colorJSON);
+    //Create synth and make audio connections
+    synthandConnect();
+    //Sets the frame rate
+    p.frameRate(30);
+  };
+
+  //Canvas settings
+  function makeCanvas() {
     //Create canvas and style it in the background
     canvas = p.createCanvas(window.innerWidth, window.innerHeight);
     canvas.style("display: inherit");
@@ -49,13 +66,10 @@ var sketch = function (p) {
     canvas.style("z-index:-3");
     bkgcol = p.color("rgba(247, 242, 203, 0.1)");
     p.background(bkgcol);
+  };
 
-    // unpack the JSON object as an array
-    risoColors = Object.values(colorJSON);
-
-    // Make the volume quieter
-    Tone.Master.volume.value = volume;
-
+  //Create synth and make audio connections
+  function synthandConnect() {
     // Setup a synth with ToneJS
     synth = new Tone.Synth({
       oscillator: {
@@ -63,31 +77,34 @@ var sketch = function (p) {
       },
     });
 
-    // Wire up our nodes:
-    // synth->master
-
-    //Make audio connections
+    //Make audio connections (Wire up our nodes: synth->master)
     var feedbackDelay = new Tone.FeedbackDelay("8n", 0.6);
     synth.connect(feedbackDelay);
     synth.connect(Tone.Master);
     feedbackDelay.connect(Tone.Master);
-
-    //Sets the frame rate
-    p.frameRate(30);
   };
 
-  // On window resize, update the canvas size
+  //On window resize, update the canvas size
   p.windowResized = function () {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 
-  // Render loop that draws shapes with p5
+  //Render loop that draws shapes with p5
   p.draw = function () {};
 
+  //Draw shapes and play sound on click of body
   $("body").click(function () {
-    console.log("ciao");
-    chooseNote();
-    drawShape();
+    var r = p.random(["sound", "shape", "soundshape"]);
+
+    if (r === "sound") {
+      chooseNote();
+    } else if (r === "shape") {
+      drawShape();
+    } else if (r === "soundshape") {
+      chooseNote();
+      drawShape();
+    } else {
+    };
   });
 
   function chooseNote() {
@@ -97,9 +114,16 @@ var sketch = function (p) {
   };
 
   function drawShape() {
+    // the user touched the screen!
+    if (!active) {
+      active = true;
+      // Clear background to white to create an initial flash
+      p.background(bkgcol);
+    };
     const dim = p.min(p.width, p.height);
     const x = p.mouseX;
     const y = p.mouseY;
+
     //Retrieves data from JSON and turn it into text
     p.noStroke();
     const curColorData = p.random(risoColors);
@@ -110,12 +134,25 @@ var sketch = function (p) {
     // background(curColor, 0.1); 
     // curColor.setAlpha(0.1); 
 
+    //Draws text
     p.fill(curColor);
     p.textAlign(p.CENTER, p.CENTER);
     p.textFont("arial");
-
-    // p.text(curColorData.pantone, x, y + size / 2 + 20); 
     p.text(curColorData.name, x, y - size / 2 - 20);
+
+    // Draw a basic polygon, handles triangles, squares, pentagons, etc
+    p.polygon = function (x, y, radius, sides = 3, angle = 0) {
+      p.beginShape();
+      for (let i = 0; i < sides; i += 1) {
+        const a = angle + p.TWO_PI * (i / sides);
+        let sx = x + p.cos(a) * radius;
+        let sy = y + p.sin(a) * radius;
+        p.vertex(sx, sy);
+      }
+      p.endShape(p.CLOSE);
+    };
+
+    //Sets probability of which shape will appear
     if (type === "circle") {
       p.ellipseMode(p.CENTER);
       p.circle(x, y, size);
@@ -125,36 +162,14 @@ var sketch = function (p) {
       p.polygon(x, y, size * 0.5, 2, p.random(-1, 1) * p.PI * 2);
     } else if (type === "polygon") {
       p.polygon(x, y, size * 0.5, p.floor(p.random(3, 10)), p.random(-1, 1) * p.PI * 2);
-    }
-  };
-/* 
-  // Update mouse position and play a sound
-  window.addEventListener("touchstart", function () {
-    // the user touched the screen!
-    if (!active) {
-      active = true;
-      // Clear background to white to create an initial flash
-      p.background(bkgcol);
     };
-  });
- */
-  // Draw a basic polygon, handles triangles, squares, pentagons, etc
-  p.polygon = function (x, y, radius, sides = 3, angle = 0) {
-    p.beginShape();
-    for (let i = 0; i < sides; i += 1) {
-      const a = angle + p.TWO_PI * (i / sides);
-      let sx = x + p.cos(a) * radius;
-      let sy = y + p.sin(a) * radius;
-      p.vertex(sx, sy);
-    }
-    p.endShape(p.CLOSE);
   };
-
 };
 
-//Start tone and p5
-$("#myButton").click(function (e) {
-  StartAudioContext(Tone.context, "#playBut").then(function (e) {
+//Starts Tone and p5
+$("#myButton").click(function () {
+  StartAudioContext(Tone.context, "#playBut").then(function () {
     myp5 = new p5(sketch);
   });
+  return false;
 });
